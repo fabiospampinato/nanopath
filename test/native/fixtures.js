bbccfffca8d6b047f04573f68d1a787f408e40ff
Fabio Spampinato <spampinabio@gmail.com>
2022-04-01 23:36:00 +0100

    Added a native test suite

---
+++ test/native/fixtures.js
@@ -0,0 +1,83 @@
+
+/* MAIN */
+
+const Fixtures = {
+
+  posix: [
+    '',
+    '/',
+    '/foo',
+    '/foo/bar',
+    '/foo/.',
+    '/foo/..',
+    '/foo/.bar',
+    '/foo/.bar.baz',
+    '/foo/bar.baz',
+    '/foo/bar.baz.qux',
+    '/foobar/baz.qux',
+    '/foobar/baz..qux',
+    '/foobar/baz..qux.',
+    '/foobar/baz..qux..',
+    '/foobar/baz.',
+    '/foobar/baz..',
+    '/foobar/baz...',
+    './foo/',
+    '../foo',
+    '.../foo',
+    '../../foo',
+    '///',
+    '/foo///bar',
+    '/foo/bar//',
+    '/foo/bar///',
+    '/foo/bar////',
+    '///foo/bar////baz',
+    '/path/to/..ext',
+    '../',
+    '///..//./foo/.//bar',
+    'bar/foo../..',
+    '../../../foo/../../../bar',
+    'foo/..'
+  ],
+
+  win32: [
+    '',
+    '\\',
+    '\\foo',
+    '\\foo\\bar',
+    '\\foo\\.',
+    '\\foo\\..',
+    '\\foo\\.bar',
+    '\\foo\\.bar.baz',
+    '\\foo\\bar.baz',
+    '\\foo\\bar.baz.qux',
+    '\\foobar\\baz.qux',
+    '\\foobar\\baz..qux',
+    '\\foobar\\baz..qux.',
+    '\\foobar\\baz..qux..',
+    '\\foobar\\baz.',
+    '\\foobar\\baz..',
+    '\\foobar\\baz...',
+    '.\\foo\\',
+    '..\\foo',
+    '...\\foo',
+    '..\\..\\foo',
+    '\\\\\\',
+    '\\foo\\\\\\bar',
+    '\\foo\\bar\\\\',
+    '\\foo\\bar\\\\\\',
+    '\\foo\\bar\\\\\\\\',
+    '\\\\\\foo\\bar\\\\\\\\baz',
+    '\\path\\to\\..ext',
+    '..\\',
+    '\\\\\\..\\\\.\\foo\\.\\\\bar',
+    'bar\\foo..\\..',
+    '..\\..\\..\\foo\\..\\..\\..\\bar',
+    'foo/..',
+    '\\\\?\\Foo\\bar.txt'
+  ]
+
+};
+
+/* EXPORT */
+
+module.exports = Fixtures;
---
+++ test/native/index.js
@@ -0,0 +1,445 @@
+
+/* IMPORT */
+
+const {describe} = require ( 'fava' );
+const path = require ( 'path' );
+const {default: Path} = require ( '../../dist' );
+const Fixtures = require ( './fixtures' );
+
+/* MAIN */
+
+describe ( 'Path', () => {
+
+  describe ( 'posix', () => {
+
+    describe ( 'basename', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.basename ( fixture );
+          const result = Path.posix.basename ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'dirname', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.dirname ( fixture );
+          const result = Path.posix.dirname ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'extname', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.extname ( fixture );
+          const result = Path.posix.extname ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'format', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.format ( path.posix.parse ( fixture ) );
+          const result = Path.posix.format ( Path.posix.parse ( fixture ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'isAbsolute', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.isAbsolute ( fixture );
+          const result = Path.posix.isAbsolute ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'join', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.join ( ...fixture.split ( '/' ) );
+          const result = Path.posix.join ( ...fixture.split ( '/' ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'normalize', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.normalize ( fixture );
+          const result = Path.posix.normalize ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'parse', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.parse ( fixture );
+          const result = Path.posix.parse ( fixture );
+
+          console.log ( i++, fixture, expected, result );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'resolve', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.resolve ( fixture );
+          const result = Path.posix.resolve ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.resolve ( ...fixture.split ( '/' ) );
+          const result = Path.posix.resolve ( ...fixture.split ( '/' ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'toNamespacedPath', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.posix.length; i < l; i++ ) {
+
+          const fixture = Fixtures.posix[i];
+          const expected = path.posix.toNamespacedPath ( fixture );
+          const result = Path.posix.toNamespacedPath ( fixture );
+
+          console.log ( i++, fixture, expected, result );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+  });
+
+  describe ( 'win32', () => {
+
+    describe ( 'basename', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.basename ( fixture );
+          const result = Path.win32.basename ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'dirname', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.dirname ( fixture );
+          const result = Path.win32.dirname ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'extname', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.extname ( fixture );
+          const result = Path.win32.extname ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'format', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.format ( path.win32.parse ( fixture ) );
+          const result = Path.win32.format ( Path.win32.parse ( fixture ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'isAbsolute', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.isAbsolute ( fixture );
+          const result = Path.win32.isAbsolute ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'join', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.join ( ...fixture.split ( '/' ) );
+          const result = Path.win32.join ( ...fixture.split ( '/' ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'normalize', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.normalize ( fixture );
+          const result = Path.win32.normalize ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'parse', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.parse ( fixture );
+          const result = Path.win32.parse ( fixture );
+
+          console.log ( i++, fixture, expected, result );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'resolve', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.resolve ( fixture );
+          const result = Path.win32.resolve ( fixture );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.resolve ( ...fixture.split ( '/' ) );
+          const result = Path.win32.resolve ( ...fixture.split ( '/' ) );
+
+          console.log ( i++, `"${fixture}"`, `"${expected}"`, `"${result}"` );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+    describe ( 'toNamespacedPath', it => {
+
+      it ( 'works like Node', t => {
+
+        for ( let i = 0, l = Fixtures.win32.length; i < l; i++ ) {
+
+          const fixture = Fixtures.win32[i];
+          const expected = path.win32.toNamespacedPath ( fixture );
+          const result = Path.win32.toNamespacedPath ( fixture );
+
+          console.log ( i++, fixture, expected, result );
+
+          t.deepEqual ( result, expected );
+
+        }
+
+      });
+
+    });
+
+  });
+
+});
